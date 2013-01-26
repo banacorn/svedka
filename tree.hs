b38f810cf35459babae10ca72277b91cc8fc0c8a
Ting-Yen LAI <banacorn@gmail.com>
2013-01-27 00:41:53 +0800

    foo

--- tree.hs
+++ tree.hs
@@ -1,5 +1,3 @@
-
-
 data Tree a = Nil | Node a (Tree a) (Tree a) deriving (Eq)
 
 data Inbalance = Balanced | LL | RR | LR | RL deriving (Show, Eq)
@@ -8,7 +6,7 @@ data Inbalance = Balanced | LL | RR | LR | RL deriving (Show, Eq)
 (+++) = insert
 
 instance (Show a, Eq a) => Show (Tree a) where
-    show tree = draw tree (height tree)
+    show tree = draw tree (height tree) ++ "\n pre: " ++ show (preorder tree) ++ "\n in: " ++ show (inorder tree)
         where
             h = height tree
             draw tree 0 = drawLevel tree (h)
@@ -57,22 +55,32 @@ search (Node a l r) b
     | a >  b = search l b
     | a <  b = search r b
 
+{-# INLINE height #-}
 height :: Tree a -> Int
 height Nil = 0
 height (Node _ l r) = 1 + max (height l) (height r)
 
 balanced :: Tree a -> Inbalance
 balanced Nil = Balanced
-balanced (Node _ Nil r)      
-    | height r < 2 = Balanced
-    | height r >= 2 && height rl > height rr = RL
-    | height r >= 2 && height rr > height rl = RR
-    where (Node _ rl rr) = r
+balanced (Node _ Nil Nil) = Balanced
+balanced (Node _ Nil r)
+    | heightR < 2 = Balanced
+    | heightR >= 2 && heightRL > heightRR = RL
+    | heightR >= 2 && heightRR > heightRL = RR
+    where   (Node _ rl rr) = r
+            heightRL = height rl
+            heightRR = height rr
+            heightR = 1 + max heightRR heightRL
+
 balanced (Node _ l Nil) 
-    | height l < 2 = Balanced
-    | height l >= 2 && height ll > height lr = LL
-    | height l >= 2 && height lr > height ll = LR
-    where (Node _ ll lr) = l
+    | heightL < 2 = Balanced
+    | heightL >= 2 && heightLL > heightLR = LL
+    | heightL >= 2 && heightLR > heightLL = LR
+    where   (Node _ ll lr) = l
+            heightLL = height ll
+            heightLR = height lr
+            heightL = 1 + max heightLR heightLL
+
 balanced (Node _ (Node a ll lr) (Node b rl rr))
     | difference heightL heightR < 2 = Balanced
     | heightL - heightR > 1 && heightLL > heightLR = LL
@@ -80,12 +88,20 @@ balanced (Node _ (Node a ll lr) (Node b rl rr))
     | heightR - heightL > 1 && heightRR > heightRL = RR
     | heightR - heightL > 1 && heightRL > heightRR = RL
     where   difference a b = abs (a - b)
-            heightL = height (Node a ll lr)
-            heightR = height (Node b rl rr)
             heightLL = height ll
             heightLR = height lr
             heightRR = height rr
             heightRL = height rl
+            heightL = 1 + max heightLL heightLR
+            heightR = 1 + max heightRL heightRR
+
+preorder :: Tree a -> [a]
+preorder Nil = []
+preorder (Node a l r) = a : preorder l ++ preorder r
+
+inorder :: Tree a -> [a]
+inorder Nil = []
+inorder (Node a l r) = inorder l ++ [a] ++ inorder r
 
 insert :: (Eq a, Ord a) => Tree a -> a -> Tree a
 insert Nil a = Node a Nil Nil
@@ -108,6 +124,14 @@ rotateLL (Node x (Node y l b) a) = (Node y l (Node x b a))
 rotateRR (Node x a (Node y b r)) = (Node y (Node x a b) r)  
 rotateLR (Node x (Node y b (Node c l r)) a) = (Node c (Node y b l) (Node x r a))
 rotateRL (Node x a (Node y (Node c l r) b)) = (Node c (Node x a l) (Node y r b))
+
+main = do
+    a <- return $! bana Nil 3000
+    putStrLn "done"
+
+bana tree 0 = tree
+bana tree n = (bana tree (n-1)) +++ n
+
 
 ll = Node 6 (Node 4 (Node 2 (Node 1 Nil Nil) (Node 3 Nil Nil)) (Node 5 Nil Nil)) (Node 7 Nil Nil)
 rr = Node 6 (Node 5 Nil Nil) (Node 8 (Node 7 Nil Nil) (Node 10 (Node 9 Nil Nil) (Node 11 Nil Nil))) 
